ede69f0466790d37e24b015be12e46054693fe9a
Amirul Faqih <fakiiahmad001@gmail.com>
2025-07-15 22:01:00 +0300

    Update package name to com.kiibridge.learningdownloader in build.gradle.kts and AndroidManifest.xml files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.learning_downloader"
+    namespace = "com.kiibridge.learningdownloader"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.learning_downloader"
+        applicationId = "com.kiibridge.learningdownloader"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
